18a0ad3b1539ada669daa53e1d9812adec3bcc56
hasanzaidi1 <hasanammarza@usf.edu>
2024-03-16 01:37:41 -0400

    Initial Commit

---
+++ applications/applicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  applicationsApp.swift
+//  applications
+//
+//  Created by H Z on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct applicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
